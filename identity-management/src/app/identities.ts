a4c9b4c46afeee8c8fc8772e324c87120e9508e2
Salvo <salvatore.apicella@outlook.com>
2023-08-28 22:14:58 +0100

    1.1.1

---
+++ identity-management/src/app/identities.ts
@@ -0,0 +1,11 @@
+export interface identity
+{
+    Firstname : string,
+    Lastname : string,
+    AccontName : string,
+    PersonalNumber : number,
+    Department : string,
+    EntryDate : Date,
+    ExitDate? : Date
+
+}
