bbf9a6a6a48e6fdd34b12167a10eb8e484a35203
Salvo <salvatore.apicella@outlook.com>
2023-08-28 22:14:43 +0100

    1.1

--- identity-management/src/app/app.component.ts
+++ identity-management/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import { identity } from './identities';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent 
+{
   title = 'identity-management';
+
+  Utente : identity[] =
+  [{
+    Firstname : "Salvatore",
+    Lastname : "Apicella",
+    AccontName : "Sapicella",
+    PersonalNumber : 3333333333,
+    Department : "IT",
+    EntryDate : new Date('22/11/2023'),
+    ExitDate : new Date('22/11/2024')
+  },
+  {
+    Firstname : "Piero",
+    Lastname : "Rossi",
+    AccontName : "Prossi",
+    PersonalNumber : 3333333333,
+    Department : "HR",
+    EntryDate : new Date('22/04/2023')}
+  ];
 }
